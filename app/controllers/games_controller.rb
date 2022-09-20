ab26efa7422549831f1929575bc11a9563122c9f
Evan Rosson <rossonevan7281@gmail.com>
2022-09-20 15:18:10 -0500

    Added user auth

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 class GamesController < ApplicationController
 
+    skip_before_action :authenticate_user
+
     def api 
         r = RestClient.get('https://www.freetogame.com/api/games')
         render json: r.body
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :games, through: :reviews
 
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true
+
 end
