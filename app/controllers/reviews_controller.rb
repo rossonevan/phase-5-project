d26bf54aa2e9202946b32c1d631a2c5eeaaa0d2f
Evan Rosson <rossonevan7281@gmail.com>
2022-09-21 10:23:47 -0500

    Added CRUD methods in the review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,44 @@
 class ReviewsController < ApplicationController
+
+    skip_before_action :authenticate_user, only: :show
+
+
+    def index
+        reviews = Review.all
+        render json: reviews, status: :ok
+    end 
+
+    def show
+        review = find_review
+        render json: review, status: :ok
+    end
+
+    def create
+        review = Review.create!(review_params)
+        current_user.reviews << review
+        render json: review, status: :created
+    end
+
+    def update
+        review = find_review
+        review.update!(review_params)
+        render json: review, status: :accepted
+    end
+
+    def destroy
+        review = find_review
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_review
+        Review.find(params[:id])
+    end
+
+    def review_params
+        params.permit(:comment, :rating, :user_id, :game_id)
+    end
+
 end
