bf0378962316e0fb367876774f04aea230d069e0
Evan Rosson <rossonevan7281@gmail.com>
2022-09-28 16:10:23 -0500

    Changed h1 styling in ReviewedGames

--- client/src/components/ReviewedGames.js
+++ client/src/components/ReviewedGames.js
@@ -21,8 +21,8 @@ function ReviewedGames ({ localGames, reviews, currentUser, handleReviews, handl
 
     return (
         <div>
-            <h1 className='text-center text-6xl text-white bg-black pb-2'> Reviewed Games</h1>
-            <div className=" p- rounded-lg shadow-sm shadow-indigo-100 flex flex-wrap">
+            <h1 className='text-center text-6xl text-white underline italic' > Reviewed Games</h1>
+            <div className="p- rounded-lg shadow-sm shadow-indigo-100 flex flex-wrap">
                 {localGameComponents}
             </div>
         </div>
