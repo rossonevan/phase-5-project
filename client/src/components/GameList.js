041726c28eed444aab13f8bbfbebdf4b418387b9
Evan Rosson <rossonevan7281@gmail.com>
2022-09-30 10:17:09 -0500

    Added styling to search bar and dropdown menu

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -28,22 +28,31 @@ function GameList({games, currentUser, handleReviews, setLocalGames, change, set
             
     return (
         <div>
-            <form>
-                <input onChange={onSearch} type='text' placeholder="Search by title..." />
-            </form>
-            <label className=''>Search Genre: </label>
-            <select className='' onChange={onFilter}>
-                <option value='default'>All Games</option>
-                <option value="shooter">Shooter</option>
-                <option value="mmorpg">MMORPG</option>
-                <option value="fighting">Fighting</option>
-                <option value="moba">MOBA</option>
-                <option value="battle_royale">Battle Royale</option>
-                <option value="card_game">Card Game</option>
-                <option value="mmofps">MMOFPS</option>
-                <option value="arpg">ARPG</option>
-                <option value="strategy">Strategy</option>
-            </select>
+            <div className="flow-root">
+                <form className="float-left">  
+                    <div className="relative m-4">
+                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        </div>
+                        <input onChange={onSearch} type="search" className="block p-4 pl-10 w-96 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Search Game..." />
+                    </div>
+                </form>
+                <div className="m-4">
+                    <label className='text-white text-xl'>Search Genre: </label>
+                    <select className='rounded m-4' onChange={onFilter}>
+                        <option value="default">All Games</option>
+                        <option value="shooter">Shooter</option>
+                        <option value="mmorpg">MMORPG</option>
+                        <option value="fighting">Fighting</option>
+                        <option value="moba">MOBA</option>
+                        <option value="battle_royale">Battle Royale</option>
+                        <option value="card_game">Card Game</option>
+                        <option value="mmofps">MMOFPS</option>
+                        <option value="arpg">ARPG</option>
+                        <option value="strategy">Strategy</option>
+                    </select>
+                </div>
+            </div>
             <div className=" p- rounded-lg shadow-sm shadow-indigo-100 flex flex-wrap">
                 {gameComponents}
             </div>
