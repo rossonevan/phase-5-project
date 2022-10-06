e500479420b99db396565c8a97666cf469468a88
Evan Rosson <rossonevan7281@gmail.com>
2022-10-06 13:39:53 -0500

    Edit functionality works

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -2,10 +2,10 @@ import {useState} from 'react';
 
 function EditForm ({ handlePatch, review}) {
 
-    const {comment, rating} = review
+    // const {comment, rating} = review
     
-    const [reviewComment, setReviewComment] = useState(comment)
-    const [reviewRating, setReviewRating] = useState(0)
+    const [comment, setComment] = useState('')
+    const [rating, setRating] = useState(0)
     const [editForm, setEditForm] = useState(false)
 
     const toggleEditForm = () => {
@@ -49,8 +49,8 @@ function EditForm ({ handlePatch, review}) {
                     transition
                     ease-in-out
                     m-0
-                    focus:text-gray-700 focus:bg-gray-200 focus:border-red-600 focus:outline-none' onChange={(e) => setReviewComment(e.target.value)} value={reviewComment} name="comment" type='text' placeholder="Write your review..." maxLength='500' required />
-            <input className='rounded pl-1 m-2 w-20 bg-gray-300 border transition ease-in-out focus:bg-gray-200 focus:border-red-600 focus:outline-none' onChange={(e) => setReviewRating(e.target.value)} value={reviewRating}name="rating" type="number" min="0" max="5" step="1" placeholder="Rating" required />
+                    focus:text-gray-700 focus:bg-gray-200 focus:border-red-600 focus:outline-none' onChange={(e) => setComment(e.target.value)} name="comment" type='text' placeholder="Write your review..." maxLength='500' required />
+            <input className='rounded pl-1 m-2 w-20 bg-gray-300 border transition ease-in-out focus:bg-gray-200 focus:border-red-600 focus:outline-none' onChange={(e) => setRating(e.target.value)} name="rating" type="number" min="0" max="5" step="1" placeholder="Rating" required />
             <button type="submit" className="bg-red-500 hover:bg-red-700 text-white font-normal text-sm py-1 px-2 rounded-full">Edit</button>
         </form>
 
